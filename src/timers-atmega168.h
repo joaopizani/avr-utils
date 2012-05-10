c34886f7a01a48b5c386806a7f85785a73bc3f90
João Paulo Pizani Flor <joaopizani@gmail.com>
2012-05-10 17:30:32 -0300

    First commit of the avr-utils repo. As of now it contains some functions for configuring timers, and in the near future it shall contains helpers for unit-testing-like testing in AVR

---
+++ src/timers-atmega168.h
@@ -0,0 +1,10 @@
+#ifndef AVRUTILS_TIMERS_ATMEGA168_H
+#define AVRUTILS_TIMERS_ATMEGA168_H
+
+#include <stdint.h>
+
+
+uint8_t getTimerPrescaleBits_atmega168(uint16_t prescale);
+
+
+#endif /* AVRUTILS_TIMERS_ATMEGA168_H */
---
+++ src/timers-atmega168p.h
@@ -0,0 +1,10 @@
+#ifndef AVRUTILS_TIMERS_ATMEGA168P_H
+#define AVRUTILS_TIMERS_ATMEGA168P_H
+
+#include <stdint.h>
+
+
+uint8_t getTimerPrescaleBits_atmega168p(uint16_t prescale);
+
+
+#endif /* AVRUTILS_TIMERS_ATMEGA168P_H */
