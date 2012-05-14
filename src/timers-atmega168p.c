53f1de4403151d5685c6674c4da7be7ad18546a7
João Paulo Pizani Flor <joaopizani@gmail.com>
2012-05-14 10:13:06 -0300

    binary literals with spaces for readability didn't work out so well, so we removed the spaces

--- src/timers-atmega168p.c
+++ src/timers-atmega168p.c
@@ -3,12 +3,12 @@
 
 
 uint8_t getTimer0PrescaleBits_atmega168p(uint16_t prescale) {
-    if(prescale == 1) return 0b 00 000 001;
-    else if(prescale == 8) return 0b 00 000 010;
-    else if(prescale == 64) return 0b 00 000 011;
-    else if(prescale == 256) return 0b 00 000 100;
-    else if(prescale == 1024) return 0b 00 000 101;
-    else return 0b 00 000 000;
+    if(prescale == 1) return 0b00000001;
+    else if(prescale == 8) return 0b00000010;
+    else if(prescale == 64) return 0b00000011;
+    else if(prescale == 256) return 0b00000100;
+    else if(prescale == 1024) return 0b00000101;
+    else return 0b00000000;
 }
 
 uint8_t getTimer1PrescaleBits_atmega168p(uint16_t prescale) {
@@ -17,13 +17,13 @@ uint8_t getTimer1PrescaleBits_atmega168p(uint16_t prescale) {
 }
 
 uint8_t getTimer2PrescaleBits_atmega168p(uint16_t prescale) {
-    if(prescale == 1) return 0b 00 000 001;
-    else if(prescale == 8) return 0b 00 000 010;
-    else if(prescale == 32) return 0b 00 000 011;
-    else if(prescale == 64) return 0b 00 000 100;
-    else if(prescale == 128) return 0b 00 000 101;
-    else if(prescale == 256) return 0b 00 000 110;
-    else if(prescale == 1024) return 0b 00 000 111;
-    else return 0b 00 000 000;
+    if(prescale == 1) return 0b00000001;
+    else if(prescale == 8) return 0b00000010;
+    else if(prescale == 32) return 0b00000011;
+    else if(prescale == 64) return 0b00000100;
+    else if(prescale == 128) return 0b00000101;
+    else if(prescale == 256) return 0b00000110;
+    else if(prescale == 1024) return 0b00000111;
+    else return 0b00000000;
 }
 
